d2456daa9a8dc891876fbb9ac5197b0a0df0709b
DaniLoc <0225668@up.edu.mx>
2022-04-07 20:11:46 -0500

    subiendo

---
+++ app/src/main/java/com/example/recycledfeed_parcial2/MovieModel.kt
@@ -0,0 +1,12 @@
+package com.example.recycledfeed_parcial2
+
+class MovieModel {
+    var title: String = ""
+    var year: String = ""
+    var director: String = ""
+    var genre: String = ""
+    var language: String = ""
+    var metascore: String = ""
+    var imdbRating: String = ""
+    var imdbVotes: String = ""
+}
